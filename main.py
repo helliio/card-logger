ca6a73bfb89ef4c3112abd7eb3c51140188794f1
Liang Zhu <helliio@users.noreply.github.com>
2020-08-12 13:07:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import datetime
+import os
+
+
+def logger(message):
+    time = datetime.datetime.now().strftime("%d.%m.%y/%H:%M")
+    message = time + ";" + message
+    print(message)
+    f = open("log.txt", "a+")
+    f.write(message + "\n")
+    f.flush()
+    os.fsync(f.fileno())
+    f.close()
+
+
+def get_input_loop():
+    while True:
+        x = input()
+        logger(x)
+
+
+get_input_loop()
